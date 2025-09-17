1f367588a0ec4e6396c7d0949744e46791090ac9
Ivan Requerme <i.requerme.142487.tc@umindanao.edu.ph>
2025-09-18 01:50:19 +0800

    Delete Map and Rooms Page/Searching Room Page/script.js

--- Map and Rooms Page/Searching Room Page/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('roomsBtn').addEventListener('click', function() {
-    const arrow = document.getElementById('arrowUp');
-    if (arrow.textContent == '▲') {
-        arrow.textContent = '▼'; 
-    } else {
-        arrow.textContent = '▲'; 
-    }
-});
