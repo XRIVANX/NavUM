2d633468a4f53322cb938c2bb70064c072ca162c
deps27 <depalmajastyne@gmail.com>
2025-09-18 23:27:32 +0800

    Add files via upload

---
+++ NaviUM/script.js
@@ -0,0 +1,179 @@
+//Rooms Group 2
+//--------------------------------------------------------------------------
+const firstfloor2_1 = document.getElementById("first-floor-button-2_1");
+const secondfloor2_1 = document.getElementById("second-floor-button-2_1");
+const thirdfloor2_1 = document.getElementById("third-floor-button-2_1");
+//--------------------------------------------------------------------------
+const firstfloor2_2 = document.getElementById("first-floor-button-2_2");
+const secondfloor2_2 = document.getElementById("second-floor-button-2_2");
+const thirdfloor2_2 = document.getElementById("third-floor-button-2_2");
+//--------------------------------------------------------------------------
+const firstfloor2_3 = document.getElementById("first-floor-button-2_3");
+const secondfloor2_3 = document.getElementById("second-floor-button-2_3");
+const thirdfloor2_3 = document.getElementById("third-floor-button-2_3");
+//--------------------------------------------------------------------------
+const firstfloorlist2 = document.getElementById("first-floor-2");
+const secondfloorlist2 = document.getElementById("second-floor-2");
+const thirdfloorlist2 = document.getElementById("third-floor-2");
+//--------------------------------------------------------------------------
+//Rooms Group 3
+//--------------------------------------------------------------------------
+const firstfloor3_1 = document.getElementById("first-floor-button-3_1");
+const secondfloor3_1 = document.getElementById("second-floor-button-3_1");
+const thirdfloor3_1 = document.getElementById("third-floor-button-3_1");
+//--------------------------------------------------------------------------
+const firstfloor3_2 = document.getElementById("first-floor-button-3_2");
+const secondfloor3_2 = document.getElementById("second-floor-button-3_2");
+const thirdfloor3_2 = document.getElementById("third-floor-button-3_2");
+//--------------------------------------------------------------------------
+const firstfloor3_3 = document.getElementById("first-floor-button-3_3");
+const secondfloor3_3 = document.getElementById("second-floor-button-3_3");
+const thirdfloor3_3 = document.getElementById("third-floor-button-3_3");
+//--------------------------------------------------------------------------
+const firstfloorlist3 = document.getElementById("first-floor-3");
+const secondfloorlist3 = document.getElementById("second-floor-3");
+const thirdfloorlist3 = document.getElementById("third-floor-3");
+//--------------------------------------------------------------------------
+const firstfloor3 = document.getElementById("first-floor-button-3");
+const secondfloor3 = document.getElementById("second-floor-button-3");
+const thirdfloor3 = document.getElementById("third-floor-button-3");
+//--------------------------------------------------------------------------
+
+const firstfloor5 = document.getElementById("first-floor-button-5");
+const secondfloor5 = document.getElementById("second-floor-button-5");
+const thirdfloor5 = document.getElementById("third-floor-button-5");
+//--------------------------------------------------------------------------
+const firstfloorlist5 = document.getElementById("first-floor-5");
+const secondfloorlist5 = document.getElementById("second-floor-5");
+const thirdfloorlist5 = document.getElementById("third-floor-5");
+//--------------------------------------------------------------------------
+
+//Rooms Group 2
+//-----------------------------------------------------
+firstfloor2_1.addEventListener("click", function () {
+  firstfloorlist2.style.display = "flex";
+  secondfloorlist2.style.display = "none";
+  thirdfloorlist2.style.display = "none";
+});
+//-----------------------------------------------------
+secondfloor2_1.addEventListener("click", function () {
+  firstfloorlist2.style.display = "none";
+  secondfloorlist2.style.display = "flex";
+  thirdfloorlist2.style.display = "none";
+});
+//-----------------------------------------------------
+thirdfloor2_1.addEventListener("click", function () {
+  firstfloorlist2.style.display = "none";
+  secondfloorlist2.style.display = "none";
+  thirdfloorlist2.style.display = "flex";
+});
+//-----------------------------------------------------
+firstfloor2_2.addEventListener("click", function () {
+  firstfloorlist2.style.display = "flex";
+  secondfloorlist2.style.display = "none";
+  thirdfloorlist2.style.display = "none";
+});
+//-----------------------------------------------------
+secondfloor2_2.addEventListener("click", function () {
+  firstfloorlist2.style.display = "none";
+  secondfloorlist2.style.display = "flex";
+  thirdfloorlist2.style.display = "none";
+});
+//-----------------------------------------------------
+thirdfloor2_2.addEventListener("click", function () {
+  firstfloorlist2.style.display = "none";
+  secondfloorlist2.style.display = "none";
+  thirdfloorlist2.style.display = "flex";
+});
+//-----------------------------------------------------
+firstfloor2_3.addEventListener("click", function () {
+  firstfloorlist2.style.display = "flex";
+  secondfloorlist2.style.display = "none";
+  thirdfloorlist2.style.display = "none";
+});
+//-----------------------------------------------------
+secondfloor2_3.addEventListener("click", function () {
+  firstfloorlist2.style.display = "none";
+  secondfloorlist2.style.display = "flex";
+  thirdfloorlist2.style.display = "none";
+});
+//-----------------------------------------------------
+thirdfloor2_3.addEventListener("click", function () {
+  firstfloorlist2.style.display = "none";
+  secondfloorlist2.style.display = "none";
+  thirdfloorlist2.style.display = "flex";
+});
+//-----------------------------------------------------
+//Room Group 3
+//-----------------------------------------------------
+firstfloor3_1.addEventListener("click", function () {
+  firstfloorlist3.style.display = "flex";
+  secondfloorlist3.style.display = "none";
+  thirdfloorlist3.style.display = "none";
+});
+//-----------------------------------------------------
+secondfloor3_1.addEventListener("click", function () {
+  firstfloorlist3.style.display = "none";
+  secondfloorlist3.style.display = "flex";
+  thirdfloorlist3.style.display = "none";
+});
+//-----------------------------------------------------
+thirdfloor3_1.addEventListener("click", function () {
+  firstfloorlist3.style.display = "none";
+  secondfloorlist3.style.display = "none";
+  thirdfloorlist3.style.display = "flex";
+});
+//-----------------------------------------------------
+firstfloor3_2.addEventListener("click", function () {
+  firstfloorlist3.style.display = "flex";
+  secondfloorlist3.style.display = "none";
+  thirdfloorlist3.style.display = "none";
+});
+//-----------------------------------------------------
+secondfloor3_2.addEventListener("click", function () {
+  firstfloorlist3.style.display = "flex";
+  secondfloorlist3.style.display = "none";
+  thirdfloorlist3.style.display = "none";
+});
+//-----------------------------------------------------
+thirdfloor3_2.addEventListener("click", function () {
+  firstfloorlist3.style.display = "none";
+  secondfloorlist3.style.display = "none";
+  thirdfloorlist3.style.display = "flex";
+});
+//-----------------------------------------------------
+firstfloor3_3.addEventListener("click", function () {
+  firstfloorlist3.style.display = "flex";
+  secondfloorlist3.style.display = "none";
+  thirdfloorlist3.style.display = "none";
+});
+//-----------------------------------------------------
+secondfloor3_3.addEventListener("click", function () {
+  firstfloorlist3.style.display = "flex";
+  secondfloorlist3.style.display = "none";
+  thirdfloorlist3.style.display = "none";
+});
+//-----------------------------------------------------
+thirdfloor3_3.addEventListener("click", function () {
+  firstfloorlist3.style.display = "flex";
+  secondfloorlist3.style.display = "none";
+  thirdfloorlist3.style.display = "none";
+});
+
+firstfloor5.addEventListener("click", function () {
+  firstfloorlist5.style.display = "flex";
+  secondfloorlist5.style.display = "none";
+  thirdfloorlist5.style.display = "none";
+});
+
+secondfloor5.addEventListener("click", function () {
+  firstfloorlist5.style.display = "none";
+  secondfloorlist5.style.display = "flex";
+  thirdfloorlist5.style.display = "none";
+});
+
+thirdfloor5.addEventListener("click", function () {
+  firstfloorlist5.style.display = "none";
+  secondfloorlist5.style.display = "none";
+  thirdfloorlist5.style.display = "flex";
+});
