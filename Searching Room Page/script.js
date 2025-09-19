92bfa2ed4a1edf1d2ae73d14f965584688930e72
Ivan Requerme <i.requerme.142487.tc@umindanao.edu.ph>
2025-09-19 08:21:00 +0800

    Update script.js
    Class list Update

--- Searching Room Page/script.js
+++ Searching Room Page/script.js
@@ -1,8 +1,24 @@
 document.getElementById('roomsBtn').addEventListener('click', function() {
     const arrow = document.getElementById('arrowUp');
-    if (arrow.textContent == '▲') {
-        arrow.textContent = '▼'; 
+    const list = document.getElementById('roomsList');
+   if (list.classList.contains('show')) {
+        list.classList.remove('show');
+        arrow.textContent = '▲';
     } else {
-        arrow.textContent = '▲'; 
+          list.classList.add('show');
+        arrow.textContent = '▼';
+  }
+});
+
+
+window.addEventListener('click', function(e) {
+    const list = document.getElementById('roomsList');
+    const btn = document.getElementById('roomsBtn');
+    const arrow = document.getElementById('arrowUp');
+    if (!btn.contains(e.target) && !list.contains(e.target)) {
+        list.classList.remove('show');
+        arrow.textContent = '▲';
     }
 });
+
+
